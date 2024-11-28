7e5de804cc357e479d2bc151dec30d8d3db602ec
Lukas <lovretic2002@gmail.com>
2024-11-28 13:55:11 +0100

    Add major category display and refactor home page layout

--- src/routes/katalog/+page.svelte
+++ src/routes/katalog/+page.svelte
@@ -1,2 +1,25 @@
+<script lang="ts">
+	import MajorCategoryCard from './MajorCategoryCard.svelte';
+
+	const majorCategories = [
+		{
+			label: 'Numizmatika',
+			href: '/katalog/numizmatika',
+			imgSrc: '/264133_A cinematic close-up shot of old coins and money p_xl-1024-v1-0.png'
+		},
+		{
+			label: 'Kristali i poludrago kamenje',
+			href: '/katalog/kristali-i-poludrago-kamenje',
+			imgSrc: '/888150_A cinematic close-up shot of a collection of spark_xl-1024-v1-0.png'
+		}
+	];
+</script>
+
 <!-- TODO: this will show the main categories and redirect to further katalog/slug -->
 <a href="/katalog/root">Root</a>
+
+<section class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+	{#each majorCategories as category}
+		<MajorCategoryCard {...category} />
+	{/each}
+</section>
---
+++ src/routes/katalog/MajorCategoryCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	type MajorCategoryCardProps = {
+		label: string;
+		href: string;
+		imgSrc: string;
+		disabled?: boolean;
+	};
+
+	let { label, href, imgSrc, disabled = false }: MajorCategoryCardProps = $props();
+</script>
+
+<a {href} class="relative h-64 w-full {disabled && 'pointer-events-none'}">
+	<img src={imgSrc} alt={label} class="absolute inset-0 h-full w-full rounded-lg object-cover" />
+
+	<div
+		class="absolute inset-0 flex items-end rounded-lg bg-gradient-to-b from-black/15 to-black/75
+         transition-colors hover:from-black/10 hover:to-black/15
+        "
+	>
+		<h2 class="p-4 text-2xl font-bold text-white">{label}</h2>
+	</div>
+</a>
